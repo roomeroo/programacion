bdf6a1f9bd7aed80af8be03176c784dfc77b4f3c
roomeroo05 <roomeroo05@gmail.com>
2024-10-09 17:48:34 +0200

    actualizacion horario

--- UT1/Libro/ejercicio04/horario.java
+++ UT1/Libro/ejercicio04/horario.java
@@ -6,17 +6,17 @@ public class horario
     {   System.out.println("┌────────────┬────────────┬───────────┬────────────┬───────────┐");
         System.out.printf("│%9s   │%9s   │ %9s │ %9s  │%9s  │\n", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes");
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "Edes", "Dig", "Lmsg", "Lmsg", "BADAT");
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "edes", "dig", "lmsg", "lmsg", "badat");
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "Edes", "Badat", "prog", "Lmsg", "BADAT");
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "edes", "badat", "prog", "lmsg", "badat");
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "Edes", "Badat", "prog", "sist", "sist");
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "edes", "badat", "prog", "sist", "sos");
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "Ipei", "prog", "sist", "sist");    
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "ipei", "prog", "sist", "sist");    
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "Ipei", "Badat", "prog", "sist");
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "ipei", "badat", "prog", "sist");
         System.out.println("├────────────┼────────────┼───────────┼────────────┼───────────┤");
-        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "Ipei", "Badat", "prog", "    ");
+        System.out.printf("│%9s   │%9s   │%9s  │%9s   │%9s  │\n", "prog", "ipei", "badat", "prog", "sist");
         System.out.println("└────────────┴────────────┴───────────┴────────────┴───────────┘");
     }
 }
