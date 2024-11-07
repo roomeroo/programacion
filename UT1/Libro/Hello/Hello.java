5dab19601ab1a338813b34406fb6f1a35f999ba5
roomeroo05 <roomeroo05@gmail.com>
2024-11-07 17:17:07 +0100

    subir unas cuantas cosas

--- UT1/Libro/Hello/Hello.java
+++ UT1/Libro/Hello/Hello.java
@@ -1,4 +1,4 @@
-package programacion.UT1.Hello;
+package programacion.UT1.Libro.Hello;
 public class Hello {
     public static void main(String[] args){
         System.out.print("Hello World!");
--- UT5/ejercicio25/ejercicio25.java
+++ UT5/ejercicio25/ejercicio25.java
@@ -5,10 +5,17 @@ public class ejercicio25 {
         System.out.print("Escribe el numerin: ");
         int num = Integer.parseInt(System.console().readLine());
         int revNum = 0;
-        while(num>0) {
+        char flag = '\0';
+        boolean zero=false;
+        if(num%10 == 0)
+            zero=true;
+        while(num > 0) {
             revNum = revNum * 10 + num % 10;
             num = num/10;
         }
-        System.out.println("El numero al reves es: " + revNum);
+        if(zero == true)
+            flag = '0';
+        System.out.println("El numero al reves es: " + flag +revNum);
     }
+    /*      No maneja los casos en el que el numero empiece en 0      */
 }
