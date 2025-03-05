01c3190e826e043c4090369f65151f1b907a7c83
Adrian romero maldonado <roomeroo05@gmail.com>
2025-03-05 18:44:11 +0100

    subir muchas cosas

---
+++ UT11/Gestión usuarios/src/personal/App.java
@@ -0,0 +1,11 @@
+package personal;
+
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        Usuarios gestor = new Usuarios("../users.data");
+        System.out.println("Estos son todos los usuarios");
+        ArrayList<Usuario> usuarios = gestor.
+    }
+}
